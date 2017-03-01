2594fcdfd55f21aa692eb55c76f2f896038a6643
Maxi Schimmel <maxx@schimmel.cooptransporte.caldenoil.com>
2017-03-01 14:44:18 -0300

    Incorporando archivos de registracion de remitos y RVs

---
+++ func/asignaTanquesRemito.php
@@ -0,0 +1,94 @@
+<?php
+// asignaTanques2.php
+// muestra modal para descarga de camión y a la vez procesa esa descarga.
+include(($_SERVER['DOCUMENT_ROOT'].'/include/inicia.php'));
+fb($_POST);
+//die;
+/*
+array(
+['fecha'] => '17/01/2017'
+['remito1'] => 6
+['remito2'] => 1000857
+['op'] => 71868068
+['totalUD'] => 20999
+['inputTq2'] => 6919
+['inputTq3'] => 10999
+['inputTq5'] => 5000
+['totalEd'] => 
+['inputTq1'] =>
+// */
+if(isset($_POST['tipo'])&&$_POST['tipo']=='yer'){
+  // grabo YER, se hace antes de tener el remito
+  if(is_numeric($_POST['rv'])&&(is_numeric($_POST['yerUD'])||is_numeric($_POST['yerID'])||is_numeric($_POST['yerNS'])||is_numeric($_POST['yerNI']))){
+    $fecha1=explode('/', $_POST['fecha']);
+    $fecha=$fecha1[2].'-'.$fecha1[1].'-'.$fecha1[0];
+    $yerNS = (is_numeric($_POST['yerNS']))?$_POST['yerNS']:0;
+    $yerUD = (is_numeric($_POST['yerUD']))?$_POST['yerUD']:0;
+    $yerID = (is_numeric($_POST['yerID']))?$_POST['yerID']:0;
+    $yerNI = (is_numeric($_POST['yerNI']))?$_POST['yerNI']:0;
+    $insertYER = "INSERT INTO yer (fecha, despachos, ns, ud, ed, np, rv) VALUES ('$fecha', 0, '$yerNS', '$yerUD', '$yerID', '$yerNI', $_POST[rv]);";
+    fb($insertYER);
+    $nuevaOrden = $mysqli->query($insertYER);
+
+    if($nuevaOrden){
+      if ($mysqli->errno == 1062) {
+        echo json_encode(array('tipo'=>'yer', 'status' => 'error','message'=> 'Camión ya registrado previamente'));
+      } else {
+        echo json_encode(array('tipo'=>'yer', 'status' => 'success','message'=> 'Camión registrado'));
+      }
+    } else {
+    if ($mysqli->errno == 1062) {
+        echo json_encode(array('tipo'=>'yer', 'status' => 'error','message'=> 'Camión ya registrado previamente'));
+      } else {
+        echo json_encode(array('tipo'=>'yer', 'status' => 'error','message'=> 'Por favor complete los datos solicitados'));
+      }
+      
+    }
+    /*array(
+    ['tipo'] =>    'yer'
+    ['fecha'] =>    '19/01/2017'
+    ['remito1'] =>    6
+    ['remito2'] =>    1001182
+    ['yerUD'] =>    87.48
+    ['yerNS'] =>
+    ['yerNI'] =>
+    ['yerID'] =>
+    )*/
+  }
+} else {
+  if(isset($_POST['fecha'])){
+      // procesa camión
+      if(is_numeric($_POST['remito1'])&&is_numeric($_POST['remito2'])&&is_numeric($_POST['op'])&&(is_numeric($_POST['inputTq1'])||is_numeric($_POST['inputTq2'])||is_numeric($_POST['inputTq3'])||is_numeric($_POST['inputTq4'])||is_numeric($_POST['inputTq5'])||is_numeric($_POST['inputTq6']))){
+          $fecha1=explode('/', $_POST['fecha']);
+          $fecha=$fecha1[2].$fecha1[1].$fecha1[0];
+          // inserta ordenes
+          $sqlOrden = "INSERT INTO ordenes (op, fechaDespachoEstimada, fechaEntregada, ultimoEstado, entregado) values ('$_POST[op]', '$fecha1[2]-$fecha1[1]-$fecha1[0]', '$fecha1[2]-$fecha1[1]-$fecha1[0]', 'Pedido Entregado', 1)";
+          fb($sqlOrden);
+          $nuevaOrden = $mysqli->query($sqlOrden);
+          $idOrden = $mysqli->insert_id;
+          $inputTq4 = $_POST['totalEd']-$_POST['inputTq1'];
+          $inputTq6 = $_POST['totalUD']-$_POST['inputTq2'];
+          //substr($_POST['fecha'],-4).substr($_POST['fecha'],3,2).substr($_POST['fecha'],0,2);
+          $sql1 = "INSERT INTO recepcioncombustibles (fecha, remito1, remito2, tq1, tq2, tq3, tq4, tq5, tq6, idOrden) VALUES ('$fecha', '$_POST[remito1]', '$_POST[remito2]', '$_POST[inputTq1]', '$_POST[inputTq2]', '$_POST[inputTq3]', '$inputTq4', '$_POST[inputTq5]', '$inputTq6', '$idOrden')";
+          fb($sql1);
+          $res1 = $mysqli->query($sql1);
+          
+          // si en la RV se incluyen litros reintegrados por YER se carga
+          /*
+          if(is_numeric($_POST['yerNS'])||is_numeric($_POST['yerUD'])||is_numeric($_POST['yerNP'])||is_numeric($_POST['yerED'])){
+              $sql2 = "INSERT INTO yer (fecha, ed, ud, ns, np, idOrden) VALUES ('$fecha', '$_POST[yerED]', '$_POST[yerUD]', '$_POST[yerNS]', '$_POST[yerNP]', '$_POST[idOrden]')";
+              $res2 = $mysqli->query($sql2);
+          }*/
+          // graba
+          echo json_encode(array('tipo'=>'remito', 'status' => 'success','message'=> 'Camión registrado'));
+      } else {
+          // error
+          echo json_encode(array('tipo'=>'remito', 'status' => 'error','message'=> 'Por favor complete los datos solicitados'));
+      }
+  }
+}
+//echo $sql1;
+//echo $sql2;
+
+//print_r($articulo);
+?>
---
+++ registroRemito.php
@@ -0,0 +1,198 @@
+<?php
+$nivelRequerido = 4;
+include($_SERVER['DOCUMENT_ROOT'].'/include/inicia.php');
+$titulo="Descarga camión | YPF";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <?php include ($_SERVER['DOCUMENT_ROOT'].'/include/head.php');?>
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+      }
+    </style>
+  </head>
+
+  <body>
+    <?php include($_SERVER['DOCUMENT_ROOT'].'/include/menuSuperior.php') ?>
+    <div class="container">
+      <div class='row'>
+        <div class="col-md-6">
+          <h2>Recepción de combustible</h2>
+          <form class="form-horizontal well formDescarga" role="form" id='formDescarga' name='formDescarga'>
+          <input type='hidden' name='tipo' value='remito'/>
+          <div class="modal-body" id='formulario'>
+            <div class="form-group">
+              <label for="fecha" class="col-sm-3 control-label">Recepción</label>
+              <div class="col-sm-7 input-group">
+                <input type="text" class="form-control" id="fecha" name="fecha" required="required" data-date-format="dd/mm/yyyy" data-plus-as-tab='true'><span class="input-group-addon glyphicon glyphicon-calendar"></span>
+              </div> 
+            </div>
+            <div class="form-group">
+              <label for="remito" class="col-sm-3 control-label">Remito</label>
+              <div class="col-sm-7 input-group">
+                <input type="text" class="form-control" name="remito1" required="required" data-plus-as-tab='true' placeholder='PV'><span class="input-group-addon"></span>
+                <input type="text" class="form-control" name="remito2" required="required" data-plus-as-tab='true' placeholder='Numero'>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="remito" class="col-sm-3 control-label">OP</label>
+              <div class="col-sm-7 input-group">
+                <input type="text" class="form-control" name="op" required="required" data-plus-as-tab='true' placeholder='OP'>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="totalUD" class="col-sm-3 control-label">Ultra Diesel</label>
+              <div class="col-sm-7 input-group">
+                <input type="number" class="form-control litros ud" id='totalUd' name="totalUD" placeholder="Total" data-plus-as-tab='true' min='1000' max='40000'><span class="input-group-addon" id='inputTq6'>Tanque 6</span>
+                <input type="number" class="form-control litros ud" id='inputTq2' name="inputTq2" placeholder="Tanque 2" data-plus-as-tab='true' max='40000'><span id='sumaUltra'></span>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="inputNS" class="col-sm-3 control-label">Nafta Super</label>
+              <div class="col-sm-7 input-group">
+                <input type="number" class="form-control litros" name="inputTq3" placeholder="Tanque 3" data-plus-as-tab='true' min='1000' max='20000'>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="inputNP" class="col-sm-3 control-label">Nafta Infinia</label>
+              <div class="col-sm-7 input-group">
+                <input type="number" class="form-control litros" name="inputTq5" placeholder="Tanque 5" data-plus-as-tab='true' min='1000' max='10000'>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="totalEd" class="col-sm-3 control-label">Infinia Diesel</label>
+              <div class="col-sm-7 input-group">
+                <input type="number" class="form-control litros ed" id='totalEd' name="totalEd" placeholder="Total" data-plus-as-tab='true' min='1000' max='20000'><span class="input-group-addon" id='inputTq4'>Tanque 4</span>
+                <input type="number" class="form-control litros ed" id='inputTq1' name="inputTq1" placeholder="Tanque 1" data-plus-as-tab='true'  max='20000'>
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <div id='msgboxremito'></div>
+            <button type="submit" class="btn btn-primary" id='graba'>Graba</button>
+          </div>
+          </form>
+        </div>
+        
+        
+        <div class="col-md-5 pull-right" id='yer'>
+          <h2>YPF en Ruta</h2>
+          <form class="form-horizontal formDescarga" role="form" id='formYER' name='formYER'>
+          <input type='hidden' name='tipo' value='yer'/>
+          <div class="modal-body" id='formulario2'>
+            <div class="form-group">
+              <label for="fecha" class="col-sm-3 control-label">Salida</label>
+              <div class="col-sm-7 input-group">
+                <input type="text" class="form-control" id="fecha2" name="fecha" required="required" data-date-format="dd/mm/yyyy" data-plus-as-tab='true'><span class="input-group-addon glyphicon glyphicon-calendar"></span>
+              </div> 
+            </div>
+            <div class="form-group">
+              <label for="remito" class="col-sm-3 control-label">RV</label>
+              <div class="col-sm-7 input-group">
+                <input type="number" class="form-control" name="rv" required="required" data-plus-as-tab='true' placeholder='RV' max='9999999'>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="totalUD" class="col-sm-3 control-label">Ultra Diesel</label>
+              <div class="col-sm-7 input-group">
+                <input type="number" class="form-control litros ud" id='yerUD' name="yerUD" placeholder="Ultra" data-plus-as-tab='true' min='0' max='10000' step="0.01">
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="inputNS" class="col-sm-3 control-label">Nafta Super</label>
+              <div class="col-sm-7 input-group">
+                <input type="number" class="form-control litros" name="yerNS" placeholder="Super" data-plus-as-tab='true' min='0' max='20000' step="0.01">
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="inputNP" class="col-sm-3 control-label">Nafta Infinia</label>
+              <div class="col-sm-7 input-group">
+                <input type="number" class="form-control litros" name="yerNI" placeholder="Infinia" data-plus-as-tab='true' min='0' max='10000' step="0.01">
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="totalEd" class="col-sm-3 control-label">Infinia Diesel</label>
+              <div class="col-sm-7 input-group">
+                <input type="number" class="form-control litros ed" id='yerID' name="yerID" placeholder="Infinia Diesel" data-plus-as-tab='true' min='0' max='20000' step="0.01">
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <div id='msgboxyer'></div>
+            <button type="submit" class="btn btn-primary" id='grabaYER'>Graba</button>
+          </div>
+          </form>
+        </div>
+      </div>
+    <?php include ($_SERVER['DOCUMENT_ROOT'].'/include/footer.php')?>
+
+    </div> <!-- /container -->
+<?php include($_SERVER['DOCUMENT_ROOT'].'/include/termina.php');?>
+<script>
+$(document).ready(function() {
+  $('#fecha').datepicker({
+    autoclose: true
+  });
+   $('#fecha2').datepicker({
+    autoclose: true
+  });
+  $('.ud').change(function(){
+    if(isNaN($('#inputTq2').val())){
+      var tq2 = 0;
+    } else {
+      var tq2 = $('#inputTq2').val();
+    }
+    var tq6 = parseInt($('#totalUd').val()-tq2);
+    $('#inputTq6').html(tq6);
+  });
+  $('.ed').change(function(){
+    if(isNaN($('#inputTq1').val())){
+      var tq1 = 0;
+    } else {
+      var tq1 = $('#inputTq1').val();
+    }
+    var tq4 = parseInt($('#totalEd').val()-tq1);
+    $('#inputTq4').html(tq4);
+  });
+  var opciones= {
+    beforeSubmit: validate, //funcion que se ejecuta antes de enviar el form
+    success: mostrarRespuesta, //funcion que se ejecuta una vez enviado el formulario
+    url:       'func/asignaTanquesRemito.php',         // override for form's 'action' attribute 
+    type:      'post',       // 'get' or 'post', override for form's 'method' attribute
+    dataType:  'json'
+  };
+  //asignamos el plugin ajaxForm al formulario myForm y le pasamos las opciones
+  $('.formDescarga').ajaxForm(opciones) ; 
+
+  //lugar donde defino las funciones que utilizo dentro de "opciones"
+  function mostrarLoader(){
+      $('#loader_gif').fadeIn("slow"); //muestro el loader de ajax
+  };
+              
+  function validate(formData, jqForm, options) { 
+    // valido que todos los litros despachados se hayan asignado a tanques.
+    return true;
+  }
+
+  function mostrarRespuesta(responseText){
+    $("#loader_gif").fadeOut("slow"); // Hago desaparecer el loader de ajax
+      
+      if(responseText.status==='success'){
+        $('#formulario').html(responseText.message).fadeIn('slow');
+      } else {
+        $('#msgbox'+responseText.tipo).html(responseText.message).fadeIn('slow');
+      }
+
+      // eliminar bloque en box izquierdo
+      // mejorar efecto de feedback
+      // eliminar opcion dedistribuir el mismo camion de nuevo
+  };
+});
+  </script>
+  </body>
+</html>
